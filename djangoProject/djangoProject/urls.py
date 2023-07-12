7ed2d1cc252f96de976c33d331409d1a44acb82f
Starlight0798 <128832734+Starlight0798@users.noreply.github.com>
2023-07-13 03:30:56 +0800

    使用Django完成查询功能的Web封装

---
+++ djangoProject/djangoProject/urls.py
@@ -0,0 +1,27 @@
+"""djangoProject URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from myweb.views import page_not_found, server_error, query_page, query_all
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('query', query_page),
+    path('query_all_persons', query_all)
+]
+
+handler404 = page_not_found
+handler500 = server_error
